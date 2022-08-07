149719e44815806d2cb16c503f36092297b68d1e
apple <apple@apples-MacBook-Pro.local>
2022-08-07 20:24:01 +0300

    done

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import IceCream
 
 # Create your views here.
+def get_ice_cream(request,ice_cream_id):
+    icecream = IceCream.objects.get (id = ice_cream_id)
+    print(icecream)
+    context = {
+        "icecream": {
+            "id": icecream.id,
+            "name": icecream.name,
+            "shop": icecream.shop,
+            "stock": icecream.stock
+
+        }
+    }
+    return render (request,"ice_cream_detail.html",context)
